21b46702e69cb7245aaaa90b464641a10dc519ec
Jacob Taylor <jwtayl@deakin.edu.au>
2020-01-27 12:15:08 +0800

    ENHANCE: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#rbenv install 2.4.3
-#rbenv rehash
-#gem install bundler
-#bundle install
+rbenv install 2.4.3
+rbenv rehash
+gem install bundler
+bundle install
 printf "\n\n######################################################\n"
 printf "Doubtfire.io installed.\n"
 printf "Start the server using 'bundle exec middleman serve'\n"
