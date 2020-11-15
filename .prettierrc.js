cb6231e4764034a2f103ccf7402badaa381dceac
Ravindu Liyanapathirana <l.ravindu@hotmail.com>
2020-11-15 18:46:14 +1100

    Setup Next.js TypeScript project w/ Sass, ESLint, Prettier, EditorConfig

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  // Prettier
+  embeddedLanguageFormatting: 'auto',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  proseWrap: 'always',
+
+  // JavaScript/TypeScript
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+
+  // React
+  jsxSingleQuote: false,
+  jsxBracketSameLine: true,
+};
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const index: FC = () => {
+  return <div>Hiya!</div>;
+};
+
+export default index;
